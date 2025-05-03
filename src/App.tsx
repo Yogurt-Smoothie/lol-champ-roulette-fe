71727332cc545dc0113be94b4c54b90e11837bf1
calendar2 <zoro94317@gmail.com>
2025-05-04 00:56:48 +0900

    Feat: #1 - 다크 모드 구현 - 디렉토리 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/shared/components/theme-provider";
+import { Button } from "@/shared/components/ui/button";
+import { ModeToggle } from "@/shared/components/mode-toggle";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
-  )
+    <ThemeProvider>
+      <ModeToggle />
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
