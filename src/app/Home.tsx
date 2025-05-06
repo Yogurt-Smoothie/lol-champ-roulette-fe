f99bb7ac97c866458eadce8e0aceefbf965c2742
calendar2 <zoro94317@gmail.com>
2025-05-06 18:19:24 +0900

    Feat: 화면 분할 레이아웃 - 헤더 구현 - 푸터 구현 - 반응형 디자인 - 기본 테마 다크 모드로 변경

---
+++ src/app/Home.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/shared/components/Footer";
+import Header from "@/shared/components/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
