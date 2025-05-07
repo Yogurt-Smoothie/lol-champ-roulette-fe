1cbe6474e167f617e37d75497136771995e5ae39
차민재 <122072805+calendar2@users.noreply.github.com>
2025-05-08 00:02:53 +0900

    Feat: 쿼리 키 중앙 관리 (#9)
    - constants 디렉토리에서 쿼리 키 상수 관리

---
+++ src/constants/queryKeys.ts
@@ -0,0 +1,3 @@
+export const queryKeys = {
+  champions: ["champions"] as const,
+};
--- src/features/champ-list/model/useGetChampList.ts
+++ src/features/champ-list/model/useGetChampList.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchChampions } from "./apiChampList";
+import { queryKeys } from "@/constants/queryKeys";
 
 const useGetChampList = () => {
   return useQuery({
-    queryKey: ["champions"],
+    queryKey: queryKeys.champions,
     queryFn: fetchChampions,
     staleTime: 1000 * 60 * 60, // 1시간 동안 데이터를 신선한 상태로 유지
     gcTime: 1000 * 60 * 60 * 24, // 24시간 동안 캐시 유지
